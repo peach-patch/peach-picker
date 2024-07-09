28c3a90a130ddec29b16d3620e7a3ebc266033a1
soyoon26 <barcardi26@gmail.com>
2024-07-09 20:55:52 +0900

    reset loginLayout

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -9,36 +9,41 @@ const index = () => {
   return (
     <div className="center1">
       <Image src={peach_logo} width={300} />
-      <div className="w-1/5">
-        <input type="checkbox" className="w-5 h-5 text-gray form-checkbox" />
-        <span className="ml-2">로그인 유지</span>
-        <input type="checkbox" className="w-5 h-5 text-gray form-checkbox" />
-        <span className="ml-2">아이디 저장</span>
-        <div className=" w-full py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
+      <div className="w-1/5 min-w-60">
+        <div className="flex items-center">
+          <input type="checkbox" className="w-5 h-5 text-gray form-checkbox" />
+          <span className="ml-2 mr-3">로그인 유지</span>
+          <input type="checkbox" className="w-5 h-5 text-gray form-checkbox" />
+          <span className="ml-2">아이디 저장</span>
+        </div>
+        <div className="mt-2 flex items-center  w-full py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
           <input
             type="text"
-            className=" bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none  "
+            className=" bg-[#f8f8f8] ml-2 text-[20px] outline-none text-gray-500 "
             defaultValue="아이디"
           />
         </div>
-        <div className="my-5 w-full py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
+        <div className="my-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
           <input
             type="text"
-            className=" bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none  "
+            className=" bg-[#f8f8f8] ml-2 text-[20px] text-gray-500 outline-none  "
             defaultValue="비밀번호"
           />
         </div>
 
         <BasicBtn text={"로그인"} bgColor={"#fb5e67"} textColor={"#fff"} />
-        <div className="font-bold">회원가입 |</div>
-        <div>아이디/비밀번호 찾기</div>
+        <div className="flex m-1">
+          <div className="font-bold">회원가입 |</div>
+          <div>아이디/비밀번호 찾기</div>
+        </div>
       </div>
-      <div className="center1">
-        <div>소셜 계정으로 간편 로그인</div>
+      <div className="m-5 center1">
+        <div className="flex-grow h-px bg-gray-400"></div>
+        <div className="text-gray">소셜 계정으로 간편 로그인</div>
         <div className="flex">
-          <Image src={kakao} width={50} />
-          <Image src={google} width={50} />
-          <Image src={naver} width={50} />
+          <Image src={kakao} width={50} className="m-3" />
+          <Image src={google} width={50} className="m-3" />
+          <Image src={naver} width={50} className="m-3" />
         </div>
       </div>
     </div>
