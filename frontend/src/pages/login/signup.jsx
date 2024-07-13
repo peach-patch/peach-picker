0a9c5729b01605d6502e09b3f2203aa9e76f23a8
soyoon26 <barcardi26@gmail.com>
2024-07-13 22:37:32 +0900

    feat: set signupLayout

--- frontend/src/pages/login/signup.jsx
+++ frontend/src/pages/login/signup.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import BasicBtn from "../../components/button/BasicBtn";
+import Image from "next/image";
+import peach_logo from "../../../public/peach_logo.png";
+import kakao from "../../images/kakao_login.png";
 const signup = () => {
   return (
-    <div className="flex flex-col justify-center items-center ">
-      <div className="w-1/5 min-w-60">
-        <div className="w-full flex items-center">
+    <div className="flex flex-col justify-center mb-10 items-center ">
+      <Image src={peach_logo} width={200} />
+      <div className="w-1/5 mt-10 min-w-60">
+        <div className="mb-5  w-full justify-center flex items-center py-3 border-[1px] border-solid border-[#808080]">
+          <Image src={kakao} width={20} />
+          <div className="ml-5">카카오 로그인</div>
+        </div>
+        <div className="w-full mb-5 flex items-center">
           <div className=" w-5/12 h-[1px] border-[1px]  border-[#808080]"></div>
-          <div className="w-2/12 text-sm text-gray-500">혹은</div>
+          <div className="w-2/12 text-sm text-gray-500 text-center">혹은</div>
           <div className=" w-5/12 border-[1px] h-[1px]  border-[#808080]"></div>
         </div>
         <div>Username</div>
-        <div className="my-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
+        <div className="mb-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
           <input
             type="text"
             className=" bg-[#f8f8f8] ml-3 text-[20px] outline-none  "
             placeholder="Enter username"
           />
         </div>
-        <div>Email</div>{" "}
-        <div className="my-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
+        <div className="">Email</div>{" "}
+        <div className="mb-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
           <input
             type="text"
             className=" bg-[#f8f8f8] ml-3 text-[20px] outline-none  "
@@ -26,7 +34,7 @@ const signup = () => {
           />
         </div>{" "}
         <div>Password</div>
-        <div className="my-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
+        <div className="mb-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
           <input
             type="text"
             className=" bg-[#f8f8f8] ml-3 text-[20px] outline-none  "
@@ -34,7 +42,7 @@ const signup = () => {
           />
         </div>{" "}
         <div>Confirm Password</div>
-        <div className="my-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
+        <div className="mb-5 w-full flex items-center py-3 bg-[#f8f8f8] border-[1px] border-solid border-[#808080]">
           <input
             type="text"
             className=" bg-[#f8f8f8] ml-3 text-[20px] outline-none  "
