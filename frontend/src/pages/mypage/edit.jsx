dd8b7c7e1c6c9e089480d9bf838f212a87803f67
soyoon26 <barcardi26@gmail.com>
2024-08-06 14:54:21 +0900

    feat: 회원정보 수정시 null 비허용

--- frontend/src/pages/mypage/edit.jsx
+++ frontend/src/pages/mypage/edit.jsx
@@ -12,6 +12,8 @@ export default function Edit() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -21,13 +23,10 @@ export default function Edit() {
   }, [initialize, isInitialized]);
 
   useEffect(() => {
-    if (isInitialized) {
-      if (!isLoggedIn) {
-        setMessage("로그인이 필요합니다.");
-        router.push("/login");
-        return;
-      }
-
+    if (isInitialized && !isLoggedIn) {
+      setMessage("로그인이 필요합니다.");
+      router.push("/login");
+    } else if (isInitialized && isLoggedIn) {
       const fetchProfile = async () => {
         try {
           const response = await fetch("/api/users/profile", {
@@ -57,9 +56,25 @@ export default function Edit() {
   }, [isInitialized, isLoggedIn, token, router]);
 
   const handleUpdateProfile = async () => {
+    if (!username) {
+      setUsernameMessage("사용자 이름을 입력해야 합니다.");
+      return;
+    } else {
+      setUsernameMessage("");
+    }
+
+    if (!password) {
+      setPasswordMessage("비밀번호를 입력해야 합니다.");
+      return;
+    } else {
+      setPasswordMessage("");
+    }
+
     if (password !== confirmPassword) {
       setMessage("비밀번호가 일치하지 않습니다.");
       return;
+    } else {
+      setMessage("");
     }
 
     try {
@@ -106,17 +121,25 @@ export default function Edit() {
           <div className="absolute left-0 w-full h-[70px] bg-[#f8f8f8] border-[1px] border-solid border-[#808080]"></div>
           <input
             type="text"
-            className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none  defaultValue-black"
+            className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              setUsernameMessage("");
+            }}
           />
+          {usernameMessage && (
+            <div className="absolute left-[27px] top-[70px] text-red-500 text-[10px]">
+              {usernameMessage}
+            </div>
+          )}
         </div>
         <div className="mt-4 mb-1 w-[112px] text-[20px]">Email</div>
         <div className="relative w-full h-[70px] flex flex-col">
           <div className="absolute left-0 w-full h-[70px] bg-[#f8f8f8] border-[1px] border-solid border-[#808080]"></div>
           <input
             type="text"
-            className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none  defaultValue-black"
+            className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             readOnly
@@ -130,18 +153,29 @@ export default function Edit() {
             className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none"
             placeholder="Enter Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setPasswordMessage("");
+            }}
           />
+          {passwordMessage && (
+            <div className="absolute left-[27px] top-[70px] text-red-500 text-[10px]">
+              {passwordMessage}
+            </div>
+          )}
         </div>
         <div className="mt-4 mb-1 w-[200px] text-[20px]">Confirm Password</div>
         <div className="relative w-full h-[70px] flex flex-col">
           <div className="absolute left-0 w-full h-[70px] bg-[#f8f8f8] border-[1px] border-solid border-[#808080]"></div>
           <input
             type="password"
-            className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none  defaultValue-black"
+            className="absolute bg-[#f8f8f8] top-[50%] transform -translate-y-1/2 left-[27px] text-[20px] outline-none"
             placeholder="Confirm Password"
             value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+              setMessage("");
+            }}
           />
         </div>
         {message && (
