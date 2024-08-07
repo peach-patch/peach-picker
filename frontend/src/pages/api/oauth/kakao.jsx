36c33904e85be6f92ee3e4b98c16b7adfa7dbfe5
soyoon26 <barcardi26@gmail.com>
2024-08-08 00:26:33 +0900

    feat: 소셜 로그인 test

--- frontend/src/pages/api/oauth/kakao.jsx
+++ frontend/src/pages/api/oauth/kakao.jsx
@@ -1,35 +1,33 @@
-// pages/api/oauth/kakao.js
+// /pages/api/oauth/kakao.js
 
 export default async function handler(req, res) {
-  console.log("Handler 시작");
+  console.log("API 핸들러 시작");
 
   if (req.method !== "POST") {
-    console.log("잘못된 메서드:", req.method);
-    return res.status(405).json({ message: "Method not allowed 확인코드" });
+    return res.status(405).json({ message: "Method not allowed" });
   }
 
   const { code, client_env } = req.body;
-  console.log("코드:", code, "클라이언트 환경:", client_env);
+  console.log("Received code:", code, "Client environment:", client_env);
 
   const bodyParams = {
     grant_type: "authorization_code",
     client_id: process.env.NEXT_PUBLIC_KAKAO_JS_KEY,
-    redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URL,
+    redirect_uri: process.env.REDIRECT_URL,
     code,
   };
 
-  // 로컬 환경인 경우에만 client_env 추가
   if (process.env.CLIENT_ENV === "dev" && client_env) {
     bodyParams.client_env = client_env;
   }
 
-  console.log("바디 파람 확인", bodyParams);
+  console.log("Body Params:", bodyParams);
 
   try {
     const response = await fetch("https://kauth.kakao.com/oauth/token", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams(bodyParams),
     });
