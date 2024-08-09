30106d3bab78770e086dedb3f3a333a18e27971d
brown <brown@musma.net>
2024-08-09 11:23:32 +0900

    Feat: 회원가입시 이메일 인증 체크 로직 추가

--- backend/src/main/java/com/peach/backend/domain/user/service/CreateUserService.java
+++ backend/src/main/java/com/peach/backend/domain/user/service/CreateUserService.java
@@ -1,9 +1,11 @@
 package com.peach.backend.domain.user.service;
 
+import com.peach.backend.domain.user.dto.mail.EmailVerifyingDto;
 import com.peach.backend.domain.user.dto.req.SignUpReq;
 import com.peach.backend.domain.user.entity.repository.UserRepository;
 import com.woo.exception.util.BizException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,14 @@ public class CreateUserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, Object> redisTemplate;
 
 
     public void createUser(final SignUpReq req) {
         if (duplicateEmailCheck(req.getEmail())) throw new BizException("user_already_exist");
+
+        EmailVerifyingDto emailVerifyingDto = (EmailVerifyingDto) redisTemplate.opsForValue().get("verify:" + req.getEmail());
+        if(emailVerifyingDto == null || !emailVerifyingDto.getStatus()) throw new BizException("email_not_verified");
 
         userRepository.save(req.toEntity(passwordEncoder));
     }
