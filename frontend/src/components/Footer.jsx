1e47aef86f6bc1ace606dc2d475f0ffdca2e50b5
soyoon26 <barcardi26@gmail.com>
2024-08-14 12:20:28 +0900

    feat: LastView 수정

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="w-screen mt-auto ">
-      <section className="p-10 bg-gray-300">
-        <div>공지사항 이용약관 개인정보처리방침 공식 인스타그램</div>
+      <section className="p-8 bg-white">
         <div>Copyright © PEACH-PATCH DEPARTMENT. All rights reserved.</div>
       </section>
     </div>
