8748ab3da61e1536a4d98a1720dec1a8fb50d8e0
soyoon26 <barcardi26@gmail.com>
2024-08-19 23:09:07 +0900

    feat: add pdf

--- frontend/src/pages/completedDrawings/index.jsx
+++ frontend/src/pages/completedDrawings/index.jsx
@@ -1,78 +1,238 @@
 import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { usePagination, useTable } from "react-table";
 
-const testData = [
-  { no: 1, date: new Date(), company: "SOSO", name: "YOYO", winner: 5 },
-  { no: 2, date: new Date(), company: "SOSO", name: "YOYO", winner: 5 },
-  { no: 3, date: new Date(), company: "SOSO1234", name: "YOYO", winner: 5 },
-  {
-    no: 4,
-    date: new Date(),
-    company: "SOSOYOYO이게 진짜 길면",
-    name: "YOYO",
-    winner: 5,
-  },
-  { no: 5, date: new Date(), company: "TOTO로의 숲", name: "ZAZA", winner: 6 },
-];
+export default function MyList() {
+  const [data, setData] = useState([]);
+  const [filterInput, setFilterInput] = useState("");
+  const [selectedFilter, setSelectedFilter] = useState("title");
+  const [startDate, setStartDate] = useState("2024-07-01");
+  const [endDate, setEndDate] = useState("2024-12-08");
 
-const formatDate = (date) => {
-  const options = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-    timeZone: "Asia/Seoul",
+  const fetchData = async (searchParams) => {
+    try {
+      const query = new URLSearchParams(searchParams).toString();
+      const response = await fetch(`/api/drawing?${query}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      console.log(result, "결과확인");
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
-  return new Intl.DateTimeFormat("ko-KR", options).format(date);
-};
+  const handleSearch = () => {
+    const searchParams = {
+      title: selectedFilter === "title" ? filterInput : "",
+      owner: selectedFilter === "owner" ? filterInput : "",
+      startDate: startDate,
+      endDate: endDate,
+    };
 
-const completedDrawings = () => {
+    fetchData(searchParams);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        accessor: "id",
+        Header: "ID",
+        Cell: ({ value }) => <div>{value}</div>,
+      },
+      {
+        accessor: "title",
+        Header: "제목",
+        Cell: ({ value }) => <div>{value}</div>,
+      },
+      {
+        accessor: "drawingAt",
+        Header: "추첨 일시",
+        Cell: ({ value }) => <div>{new Date(value).toLocaleString()}</div>,
+      },
+      {
+        accessor: "winner",
+        Header: "당첨자 수",
+        Cell: ({ value }) => <div>{value}명</div>,
+      },
+      {
+        accessor: "drawingType",
+        Header: "추첨 유형",
+        Cell: ({ value }) => <div>{value}</div>,
+      },
+      {
+        accessor: "organizer",
+        Header: "추첨 회사",
+        Cell: ({ value }) => <div>{value}</div>,
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page, // 현재 페이지의 행들
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable({ columns, data }, usePagination);
+
   return (
-    <div className="flex flex-col items-center justify-center w-full overflow-hidden">
-      <div className="w-4/5">
-        <div className="ml-4 mb-3  mt-10 left-0 text-[2vw] ">
-          과거 추첨 내역
-          <br />
-        </div>
-      </div>
-      <div className="w-4/5 h-0 border-[1px] border-solid border-[#000]"></div>
-      <div className="w-4/5">
-        <div className="flex w-full p-3 text-[1.5vw]">
-          <div className="text-center w-1/7">NO</div>
-          <div className="w-1/4 text-center ">추첨 일시</div>
-          <div className="w-1/5 text-center">회사</div>
-          <div className="w-1/5 text-center ">이벤트명</div>
-          <div className="flex-grow mr-2 text-right">당첨자 수</div>
-        </div>
-      </div>
-      <div className=" w-4/5 h-0 border-[1px] border-solid border-[#000]"></div>
-
-      {testData.map((data, index) => (
-        <Link
-          className="flex items-center justify-center w-full"
-          href={`/completedDrawings/${data.no}`}
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex mb-4">
+        <select
+          className="p-2 mr-4 border border-gray-300 rounded"
+          value={selectedFilter}
+          onChange={(e) => setSelectedFilter(e.target.value)}
         >
-          <div
-            key={index}
-            className="relative pt-2 pb-2 flex w-4/5 text-[1.5vw]"
-          >
-            <div className="ml-2 text-center w-1/7">{data.no}번</div>
-            <div className="w-1/4 text-center ">{formatDate(data.date)}</div>
-            <div className="w-1/5 text-center">{data.company}</div>
-            <div className="w-1/5 text-center ">{data.name}</div>
-            <div className="flex-grow mr-8 text-right">{data.winner}명</div>
-          </div>
-        </Link>
-      ))}
+          <option value="title">제목</option>
+          <option value="owner">추첨 회사</option>
+        </select>
 
-      <div className=" w-4/5  border-[1px] border-solid border-[#000]"></div>
-      {/* <div className="absolute left-[661px] top-[869px] text-[24px]  whitespace-nowrap">
-        1 2 3 4 5
-      </div> */}
+        <input
+          value={filterInput}
+          onChange={(e) => setFilterInput(e.target.value)}
+          placeholder="검색어 입력"
+          className="p-2 mr-4 border border-gray-300 rounded"
+        />
+
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          className="p-2 mr-4 border border-gray-300 rounded"
+        />
+
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          className="p-2 mr-4 border border-gray-300 rounded"
+        />
+
+        <button
+          onClick={handleSearch}
+          className="p-2 text-white bg-blue-500 rounded"
+        >
+          검색
+        </button>
+      </div>
+      <div className="flex items-center content-end justify-between w-4/5">
+        <div className="pb-1 pl-1 text-left">완료된 추첨 내역</div>
+      </div>
+      <table {...getTableProps()} className="w-4/5">
+        <colgroup>
+          <col style={{ width: "5%", minWidth: "100px" }} />
+          <col style={{ width: "10%", minWidth: "50px" }} />
+          <col style={{ width: "20%", minWidth: "10px" }} />
+          <col style={{ width: "35%", minWidth: "150px" }} />
+          <col style={{ width: "10%", minWidth: "10px" }} />
+        </colgroup>
+        <thead>
+          {headerGroups.map((headerGroup) => {
+            const { key, ...restHeaderGroupProps } =
+              headerGroup.getHeaderGroupProps();
+            return (
+              <tr
+                key={key}
+                {...restHeaderGroupProps}
+                className="border-t-2 border-b-2 border-black"
+              >
+                {headerGroup.headers.map((column) => {
+                  const { key, ...restColumnProps } = column.getHeaderProps();
+                  return (
+                    <th key={key} {...restColumnProps} className="px-4 py-2">
+                      {column.render("Header")}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, rowIndex) => {
+            prepareRow(row);
+
+            const isLastRow = rowIndex === page.length - 1;
+            const { key, ...restRowProps } = row.getRowProps();
+            return (
+              <tr
+                key={key}
+                {...restRowProps}
+                className={` ${isLastRow ? "border-b-2 border-black" : ""}`}
+              >
+                {row.cells.map((cell) => {
+                  const { key, ...restCellProps } = cell.getCellProps();
+                  return (
+                    <td
+                      key={key}
+                      {...restCellProps}
+                      className="px-4 py-2 text-center"
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="mt-10">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[5, 10, 20].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              {pageSize}개씩 보기
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
-};
-
-export default completedDrawings;
+}
