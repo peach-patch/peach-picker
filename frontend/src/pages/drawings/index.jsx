76de3f26c6d3067afbbc348ff56d2ecb9fb8e386
soyoon26 <barcardi26@gmail.com>
2024-08-22 22:48:48 +0900

    feat: 썸네일  추가

--- frontend/src/pages/drawings/index.jsx
+++ frontend/src/pages/drawings/index.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function index() {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/drawing`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Network response was not ok: ${response.status} - ${errorText}`
+        );
+      }
+
+      const result = await response.json();
+      setData(result);
+      setFilteredData(result); // 초기 필터링
+      console.log(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // 초기 렌더링
+  }, []);
+
   return <div>index</div>;
 }
