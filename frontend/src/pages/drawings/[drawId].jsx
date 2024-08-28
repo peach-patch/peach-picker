10590fbd7ce005dec36a835ab2bc24cc16023b4a
soyoon26 <barcardi26@gmail.com>
2024-08-28 16:43:19 +0900

    feat: 등록 수정

--- frontend/src/pages/drawings/[drawId].jsx
+++ frontend/src/pages/drawings/[drawId].jsx
@@ -110,6 +110,7 @@ export default function DrawId() {
         `${process.env.NEXT_PUBLIC_API_URL}/drawing/${drawId}`
       );
       setWinners(updatedData.data.winners || []);
+      console.log(updatedData.data);
       console.log(winners, "이게 나와라");
     } catch (error) {
       console.error("Error starting drawing:", error);
@@ -184,20 +185,47 @@ export default function DrawId() {
           </div>
 
           <article className="w-full p-4 overflow-y-auto bg-white border-2 border-gray-400 dark:bg-gray-700 dark:border-gray-600 rounded-lg max-h-40">
-            <dt className="mb-2 text-xl font-semibold dark:text-gray-100">
-              &lt;응모자 목록&gt;
-            </dt>
-            {winners.length > 0 ? (
-              winners.map((winner, index) => (
-                <dd
-                  key={index}
-                  className="m-2 text-gray-700 dark:text-gray-300"
-                >
-                  {winner.name} ({winner.phone}) - 당첨자
-                </dd>
-              ))
+            {/* <dt className="mb-2 text-xl font-semibold dark:text-gray-100">
+              &lt;응모자 및 당첨자 목록&gt;
+            </dt> */}
+
+            {data.drawingAt && new Date(data.drawingAt) > new Date() ? (
+              <>
+                <p className="mb-2 text-lg dark:text-gray-100">
+                  &lt;응모자 목록&gt;
+                </p>
+                {data.participants && Array.isArray(data.participants) ? (
+                  data.participants.map((participant, index) => (
+                    <dd
+                      key={index}
+                      className="m-2 text-gray-700 dark:text-gray-300"
+                    >
+                      {participant.name} ({participant.email}) -{" "}
+                      {participant.phone}
+                    </dd>
+                  ))
+                ) : (
+                  <p>응모자 정보가 없습니다.</p>
+                )}
+              </>
             ) : (
-              <p>응모자 정보가 없습니다.</p>
+              <>
+                <p className="mb-2 text-lg dark:text-gray-100">
+                  &lt;당첨자 목록&gt;
+                </p>
+                {winners.length > 0 ? (
+                  winners.map((winner, index) => (
+                    <dd
+                      key={index}
+                      className="m-2 text-gray-700 dark:text-gray-300"
+                    >
+                      {winner.name} ({winner.phone}) - 당첨자
+                    </dd>
+                  ))
+                ) : (
+                  <p>당첨자 정보가 없습니다.</p>
+                )}
+              </>
             )}
           </article>
         </section>
